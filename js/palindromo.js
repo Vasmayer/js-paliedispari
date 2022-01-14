e8e540d38360d18462c15ee16c7fc05cd21e3c58
Vasmayer <lealimarco90@gmail.com>
2022-01-14 15:36:17 +0100

    basic structure to start

---
+++ js/palindromo.js
@@ -0,0 +1,9 @@
+/* 
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+
+BONUS:
+Usiamo il DOM per stampare e chiedere le informazioni all'utente!
+
+*/

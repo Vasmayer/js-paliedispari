2aff89158d68b31f5a69eb8b3bc3f4589961bfb1
Vasmayer <lealimarco90@gmail.com>
2022-01-14 16:27:06 +0100

    es palindromo completato

--- js/palindromo.js
+++ js/palindromo.js
@@ -7,3 +7,51 @@ BONUS:
 Usiamo il DOM per stampare e chiedere le informazioni all'utente!
 
 */
+
+
+const verifyButton = document.getElementById('palindromebutton');
+const inputWord = document.getElementById('input-word');
+const response = document.getElementById('response');
+
+console.log(inputWord);
+
+function isPalindrome (input)
+{
+    const word = input.value;
+    console.log(word);
+
+    const lenght = word.length
+
+    if(lenght !== 0)
+    {          
+            const firstHalfWord = word.substring(0,lenght/2);
+            const lastHalfWord = word.substring(lenght/2,lenght);
+            console.log(firstHalfWord);
+            console.log(lastHalfWord);
+
+            if(firstHalfWord === lastHalfWord)
+            {
+                return true;
+            }
+
+            return false;
+    }
+    else
+    {
+        alert('Devi inserire una parola!');
+        return false;
+    }
+}
+
+
+verifyButton.addEventListener('click',function(){
+
+    if(isPalindrome(inputWord))
+    {
+        response.innerHTML = "<span class='text-success'>La parola è PALINDROMA</span>"
+    }
+    else
+    {
+        response.innerHTML = "<span class='text-secondary'>La parola NON è PALINDROMA</span>"   
+    }
+});
